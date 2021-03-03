b0dca5cba1d879fc8d1ce7900c6845cd2ea6c3d0
cyd3r <cyd3rhacker@gmail.com>
2021-03-03 15:39:54 +0100

    remove pug dependency

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,39 @@
 
 const robot = require('robotjs')
 const WebSocket = require('ws')
-
-const startFrontendServer = require('./serve_frontend')
+const express = require('express')
+const qr = require('qrcode')
+const open = require('open')
+const os = require('os')
 
 function acceleratedDelta(body, sensitivity) {
     return {
         x: Math.sign(body.deltaX) * Math.ceil(body.deltaX * sensitivity * body.velocityX),
         y: Math.sign(body.deltaY) * Math.ceil(body.deltaY * sensitivity * body.velocityY),
     }
+}
+
+function getMyIP() {
+    // what's my ip
+    // inspired by https://stackoverflow.com/a/8440736
+    const addresses = Object.values(os.networkInterfaces()).reduce((prev, ifaces) => {
+        const ifAddresses = ifaces
+        .map((iface) => {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return undefined
+            }
+            return iface.address
+        })
+        .filter(v => v !== undefined)
+        return [...prev, ...ifAddresses]
+    }, []);
+    if (addresses.length > 1) {
+        console.warn('Not tested')
+    } else if (addresses.length === 0) {
+        console.warn('Could not determine the address of your computer. Check your internet connection and restart the server')
+    }
+    return addresses[0]
 }
 
 const wss = new WebSocket.Server({ port: 8080 })
@@ -31,4 +56,22 @@ wss.on('connection', (ws) => {
     })
 })
 
-startFrontendServer()
+const app = express()
+const port = 3000
+
+app.use('/', express.static('frontend'))
+
+app.get('/qr', (req, res) => {
+    qr.toBuffer(`http://${getMyIP()}:${port}`, { type: 'png' })
+        .then(buffer => {
+            res.type("png")
+            res.send(buffer);
+        })
+        .catch(console.error);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+    // open a web page with instructions on how to use the software
+    open(`http://${getMyIP()}:${port}/instructions.html`)
+})
